2f3fff7d020e211a9b55d065a75bba41eb4d4255
Jamal Aha <jamalah@icloud.com>
2021-08-09 21:32:38 +0300

    1

--- main.js
+++ main.js
@@ -10,3 +10,38 @@ function buttonClick() {
         alert('Не знаете ? ECMAScript');
     }
 }
+function buttonClick2() {
+    let number$ = prompt(
+        "Введите число", ''
+        )
+    ;
+
+    if (number$ > 0) {
+        alert(1);
+    }
+    else if (number$ < 0) {
+        alert(-1);
+    }
+    else if (number$ === 0) {
+        alert(0);
+    }
+    else {
+        alert('Введите число')
+    }
+}
+function buttonClick3() {
+    let login = prompt(
+        "Логин", ''
+        )
+    ;
+
+    if ( login === 'Админ') {
+        prompt ('Пароль', '');
+    }
+    else if ( login !== 'Админ'){
+        alert('Я вас не знаю')
+    }
+    else if ( login == null ) {
+        alert('Вход отменен')
+    }
+}
